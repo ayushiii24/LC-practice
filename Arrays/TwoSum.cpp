233447d87e75ea513ff385f9eb099b97f3f8348e
ayushi <ayushijoshi2425@gmail.com>
2026-03-23 20:36:35 +0530

    Update TwoSum.cpp

--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -1,1 +1,22 @@
+//Problem 1 [Two Sum]
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n =nums.size();
+        for (int i=0;i<n;i++){
+            for (int j = i+1;j<n;j++){
+                if (nums[i] +nums[j]==target){
+                    return {i,j};
+                } 
+            }
+        } 
+    return {};  
+    }  
+};
+
+
+/*
+Time Complexity: O(n²)
+approach : brute force
+*/
