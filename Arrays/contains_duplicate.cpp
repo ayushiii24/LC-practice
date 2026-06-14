449997522950392f7dc2ca592fa4ebc445d00964
ayushi <ayushijoshi2425@gmail.com>
2026-06-14 21:15:29 +0530

    Update contains_duplicate.cpp

--- Arrays/contains_duplicate.cpp
+++ Arrays/contains_duplicate.cpp
@@ -2,6 +2,8 @@
 
 class Solution {
 public:
+    // APPROACH 1: Sorting Method (brute force)
+    // Time complexity: O(n log n)  Space complexity: O(1)
     bool hasDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         for (int i=1;i<nums.size();i++){
@@ -9,9 +11,20 @@ public:
         }
             return false;
         }
+
+    // APPROACH 2: Hash map method (optimised)
+    // Time complexity: O(n)  Space Complexity: O(n)
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map <int,int> freq;
+        for (int num : nums){
+            freq[num]++;
+        }
+        for (auto i : freq){
+            if (i.second > 1) return true;
+        }
+        return false;
+        
+    }
 };
 
-/*
-Time Complexity: O(n log n)
-approach : brute force
-*/
+
