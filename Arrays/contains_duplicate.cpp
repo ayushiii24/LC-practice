85958d99caa54c93a44d38091ba25f6e34fc62a4
ayushi <ayushijoshi2425@gmail.com>
2026-06-14 21:01:33 +0530

    Update contains_duplicate.cpp

--- Arrays/contains_duplicate.cpp
+++ Arrays/contains_duplicate.cpp
@@ -1,1 +1,17 @@
+//Problem=2 [Contains duplicate]
 
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for (int i=1;i<nums.size();i++){
+            if (nums[i]==nums[i-1]) return true;
+        }
+            return false;
+        }
+};
+
+/*
+Time Complexity: O(n log n)
+approach : brute force
+*/
