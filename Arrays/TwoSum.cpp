c73b276b93a44512fe7ac1bbb3b437c5bee7c602
ayushi <ayushijoshi2425@gmail.com>
2026-06-15 19:28:44 +0530

    Update TwoSum.cpp

--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -2,6 +2,8 @@
 
 class Solution {
 public:
+    //Time Complexity: O(n²)
+    //approach : two nested loops (brute force)
     vector<int> twoSum(vector<int>& nums, int target) {
         int n =nums.size();
         for (int i=0;i<n;i++){
@@ -13,10 +15,18 @@ public:
         } 
     return {};  
     }  
+
+    //time complexity : O(n)
+    //approach : hashmap optimised
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        for ( int i=0 ;i <nums.size();i++){
+            int req = target - nums[i];
+
+            if (map.find(req) != map.end()) return {map[req],i};
+            map[nums[i]]=i;  
+        }
+        return {};
+    }
 };
 
-
-/*
-Time Complexity: O(n²)
-approach : brute force
-*/
