7327043022f644b092fb0cc9ea2601c133ff31d1
ayushi <ayushijoshi2425@gmail.com>
2026-06-20 23:17:41 +0530

    Update rotate_array.cpp

--- Arrays/rotate_array.cpp
+++ Arrays/rotate_array.cpp
@@ -1,1 +1,13 @@
-
+//Problem :Rotate array
+class Solution {
+public:
+    //Approach : Sorting (optimised)
+    // time complexity : O(n) , space complexity : O(1)
+    void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        int x= nums.size()-k;
+        reverse(nums.begin(), nums.begin()+x);
+        reverse(nums.begin()+x,nums.end());
+        reverse(nums.begin(),nums.end());
+    }
+};
