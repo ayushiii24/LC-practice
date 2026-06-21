123f44a4e29896a69e76ec77c5766c9f8c36fb42
ayushi <ayushijoshi2425@gmail.com>
2026-06-21 12:57:18 +0530

    Update sorted_rotated.cpp

--- Arrays/sorted_rotated.cpp
+++ Arrays/sorted_rotated.cpp
@@ -1,1 +1,22 @@
+//problem : to check if array is sorted and rotated
+class Solution {
+public:
+    //approach : rotate and compare (brute force)
+    // time complexity : O(n^2) space complexity : O(n)
+    bool check(vector<int>& nums) {
+        vector <int> sorted(nums);
+        sort(sorted.begin(),sorted.end());
+        int n=nums.size();
+        for (int r=0;r<n;r++){
+            if (nums==sorted) return true;
 
+            int last = nums[n-1];
+            for (int i=n-1;i>0;i--){
+                nums[i] = nums[i-1];
+            }
+            nums[0]=last;
+        }
+        return false;
+        
+    }
+};
