9647659c9a1c78d966b8db829e1ce4d32dcdebe3
ayushi <ayushijoshi2425@gmail.com>
2026-06-23 22:12:33 +0530

    Update kth_missing.cpp

--- Arrays/kth_missing.cpp
+++ Arrays/kth_missing.cpp
@@ -1,1 +1,19 @@
-
+//problem : Kth Missing Positive Number
+class Solution {
+public:
+  // approach : Linear scan using two pointers (brute force)
+  // Time Complexity: O(n+k) , space complexity : O(k)
+    int findKthPositive(vector<int>& arr, int k) {
+        vector <int> v;
+        int i=1;
+        int curr=0;
+        while(v.size()<k){
+            if (curr >= arr.size() || i!=arr[curr]) {
+                v.push_back(i); 
+                i++;
+            }
+            else {curr++; i++;}
+        }
+        return v[k-1];
+    }
+};
